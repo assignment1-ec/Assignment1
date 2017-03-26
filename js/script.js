89066ce5827d122645b80c759c52ebc7ab466b9b
Cao Khanh Nhat <51302751@hcmut.edu.vn>
2017-03-26 18:59:23 +0700

    fix style by Nhat Cao

--- js/script.js
+++ js/script.js
@@ -5,3 +5,23 @@ $('#search-input').keypress(function(e) {
         return;
     }
 });
+
+
+$(".image-book .icon-small img").click(function() {
+    var src = $(this).attr('src');
+    $('.image-book .logo img').attr('src', src);
+})
+
+$('.info-book .change-number button:first-child').click(function() {
+    var value = $('.info-book .change-number #number').val();
+    value = parseInt(value);
+    if (value <= 0)
+        return;
+    $('.info-book .change-number #number').attr('value', value - 1);
+});
+
+$('.info-book .change-number button:last-child').click(function() {
+    var value = $('.info-book .change-number #number').val();
+    value = parseInt(value);
+    $('.info-book .change-number #number').attr('value', value + 1);
+});
