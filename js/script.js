864e1f2bfc42af189439fb149b181b304f819701
Cao Khanh Nhat <51302751@hcmut.edu.vn>
2017-03-28 07:00:01 +0700

    add notify by Nhat Cao

--- js/script.js
+++ js/script.js
@@ -30,3 +30,36 @@ $('.user-panel #edit-fullname').click(function() {
     $('.user-panel #user-fullname').attr('readonly', false);
     $('.user-panel #user-fullname').addClass('edit');
 });
+
+
+
+
+$('.button-ord').click(function() {
+    var email = $('#user-email').val();
+    location.href = "notify.html";
+    sendMail();
+});
+
+
+function sendMail() {
+    $.ajax({
+        type: 'POST',
+        url: 'https://mandrillapp.com/api/1.0/messages/send.json',
+        data: {
+            'key': 'ea6bdf6eb667052162fe952ba75994fa-us15',
+            'message': {
+                'from_email': 'swiftbook@gmail.com',
+                'to': [{
+                    'email': 'khanhnhat12@yahoo.com.vn',
+                    'name': 'RECIPIENT NAME (OPTIONAL)',
+                    'type': 'to'
+                }],
+                'autotext': 'true',
+                'subject': 'YOUR SUBJECT HERE!',
+                'html': 'YOUR EMAIL CONTENT HERE! YOU CAN USE HTML!'
+            }
+        }
+    }).done(function(response) {
+        alert(response); // if you're into that sorta thing
+    });
+}
